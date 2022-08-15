454a4cca84f32b2c6657600f941ab4e5acc863ff
LuckyLotspeich <nicklotspeich@u.boisestate.edu>
2022-08-15 09:37:13 -0600

    Test 1 Wraith Color and function

--- evidence.js
+++ evidence.js
@@ -5,3 +5,18 @@ function checkAddress(checkbox)
         alert("a");
     }
 }
+
+
+function updateEvidence() {
+    // Get the checkbox
+    var emfBox = document.getElementById("emf-box");
+    // Get the output text (This is from the sample)
+     var wraithText = document.getElementById("wraithtext");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      wraithText.style.color = "blue";
+    } else {
+        wraithText.style.color = "red";
+    }
+  }
